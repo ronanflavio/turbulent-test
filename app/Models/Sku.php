9509db1bbad5179d6904de19c9a8fbc0534af305
Ronan Flávio <ronan.flavio@hotmail.com>
2020-11-08 21:55:48 -0300

    new: model for johnnysku table

---
+++ app/Models/Sku.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sku extends Model
+{
+    use HasFactory;
+
+    protected $table = 'johnnysku';
+
+    public static function tableName()
+    {
+        return (new self())->getTable();
+    }
+}
